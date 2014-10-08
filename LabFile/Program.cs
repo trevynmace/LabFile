8750b1e338619dff8685b230a2d1a8f730bc8263
Trevyn Mace <trevynmace@gmail.com>
2014-10-08 06:18:52 -0600

    initial commit

---
+++ LabFile/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPoem("Poem.txt");
+            SavePoemWithLineNumbers();
+        }
+
+        static void PrintPoem(String fileName)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    Console.Write(reader.ReadToEnd());
+                }
+            }
+            catch(Exception ex)
+            {
+                //do something
+            }
+        }
+
+        static void SavePoemWithLineNumbers()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("Poem.txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter("Poem1.txt"))
+                    {
+                        int count = 1;
+
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            writer.WriteLine(string.Format("{0:00} {1}", count, line));
+                            count++;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                //do something
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+C# working with file IO
